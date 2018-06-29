24336a3749f615284ee45c79dc9957d98b442fa7
Maverick Tesini <mavericktesini@gmail.com>
2018-06-29 03:33:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,7 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE grade < 12
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).collect {|row| self.new_from_db(row)}
   end
 
   def save
