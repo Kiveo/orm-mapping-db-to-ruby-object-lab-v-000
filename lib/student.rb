2477dab465796552e049175888ca005082f03407
Maverick Tesini <mavericktesini@gmail.com>
2018-06-29 03:38:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,6 +58,13 @@ class Student
     DB[:conn].execute(sql).collect {|row| self.new_from_db(row)}.first
   end
 
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, x)
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
