c272cb0fe84bc0cd3d8620e58be54e3974bfb19c
Maverick Tesini <mavericktesini@gmail.com>
2018-06-29 03:35:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,6 +44,13 @@ class Student
     DB[:conn].execute(sql).collect {|row| self.new_from_db(row)}
   end
 
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 LIMIT ?
+    SQL
+    DB[:conn].execute(sql, x).collect {|row| self.new_from_db(row)}
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
